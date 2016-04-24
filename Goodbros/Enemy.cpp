6d1cf4a5006cfae7239c0cb86234604e56dbd51f
Marc Lopez <marclopez_1996@outlook.es>
2016-04-24 19:46:56 +0200

    Module enemy fixed

---
+++ Goodbros/Enemy.cpp
@@ -0,0 +1,26 @@
+#include "Application.h"
+#include "Enemy.h"
+#include "ModuleCollision.h"
+#include "ModuleRender.h"
+
+Enemy::Enemy(int x, int y) : position(x, y), collider(nullptr)
+{}
+
+Enemy::~Enemy()
+{
+	if (collider != nullptr)
+		App->collision->EraseCollider(collider);
+}
+
+const Collider* Enemy::GetCollider() const
+{
+	return collider;
+}
+
+void Enemy::Draw(SDL_Texture* sprites)
+{
+	if (collider != nullptr)
+		collider->SetPos(position.x, position.y);
+
+	App->render->Blit(sprites, position.x, position.y, &(animation->GetCurrentFrame()));
+}
---
+++ Goodbros/Enemy.h
@@ -0,0 +1,29 @@
+#ifndef __ENEMY_H__
+#define __ENEMY_H__
+
+#include "p2Point.h"
+#include "Animation.h"
+
+struct SDL_Texture;
+struct Collider;
+
+class Enemy
+{
+protected:
+	Animation* animation;
+	Collider* collider;
+
+public:
+	iPoint position;
+
+public:
+	Enemy(int x, int y);
+	virtual ~Enemy();
+
+	const Collider* GetCollider() const;
+
+	virtual void Move() {};
+	virtual void Draw(SDL_Texture* sprites);
+};
+
+#endif // __ENEMY_H__
