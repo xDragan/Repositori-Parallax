100df5cd8615286b3dfadb4075f48dbe98943eee
xDragan <marcfv97@gmail.com>
2016-04-25 20:33:19 +0200

    Stage 1 sprite clean

--- Goodbros/ModuleSceneFirst.cpp
+++ Goodbros/ModuleSceneFirst.cpp
@@ -24,7 +24,7 @@ bool ModuleSceneFirst::Start()
 {
 	LOG("Loading space scene");
 	
-	background = App->textures->Load("bloodbros/level1.png");
+	background = App->textures->Load("bloodbros/stage1_clean.png");
 	App->audio->Enable(1);
 	App->player->Enable(0);
 	App->particles->Enable(0);
